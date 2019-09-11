308b8448a428f7fbc971f0fc41e31a83d595f68c
stefan <stefan.10ro@yahoo.com>
2019-09-11 20:56:13 +0300

    com

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        // Casa
+        String casa = "Casa mea";
+        byte etaje = 2;
+        byte camere = 5;
+        float suprafata = 160.58f;
+
+
+        System.out.println(casa);
+
+
+
+
+    }
+}
